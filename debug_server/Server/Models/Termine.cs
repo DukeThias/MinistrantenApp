fcd66ee5b550ca86bbbf8f4bc943e29870a5a74b
MatthiasW <matthias-warken@t-online.de>
2025-05-07 23:47:02 +0200

    Weiter an SQL gearbeitet, initiiert

---
+++ debug_server/Server/Models/Termine.cs
@@ -0,0 +1,10 @@
+public class Termin
+{
+        public string Name { get; set; }
+        public string Beschreibung { get; set; }
+        public string Ort { get; set; }
+        public DateTime Start { get; set; }
+        public string Teilnehmer { get; set; }
+
+        
+}
