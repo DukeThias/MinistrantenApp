4e8cf1f7f14f01e795a6e601fe32af1128fac857
MatthiasW <matthias-warken@t-online.de>
2025-05-07 23:30:47 +0200

    SQL und .NET laufen

--- debug_server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ debug_server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b60c013ac9ad613877c36381568fdddd974e407")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e79e09ae2b3f2e64a9caa206007babfc094d4a5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
