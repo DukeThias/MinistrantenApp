32a3bdeafe4dbcca2765c939f72dc08fedf4cf55
MatthiasW <matthias-warken@t-online.de>
2025-05-07 23:48:12 +0200

    Alles hinzugefügt

--- debug_server/Server/Program.cs
+++ debug_server/Server/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=datenbank.db"));
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
