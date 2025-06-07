0bf787bf7dbc56b474bf4ce4ed49599cfef00b98
DukeThias <matthias-warken@t-online.de>
2025-06-07 16:59:19 +0200

    keine ahnung gradle repariert

--- miniapp_2/android/app/build.gradle.kts
+++ miniapp_2/android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 android {
     namespace = "com.example.miniapp_2"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- miniapp_2/ios/Flutter/flutter_export_environment.sh
+++ miniapp_2/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/lukas/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/lukas/Dokumente/Ministranten/miniapp_2"
+export "FLUTTER_ROOT=/home/matthias/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/matthias/Miniapp/miniapp_2"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
